681731397bd449b1fefe12701b40d4bfc2ed9bb1
Leone-Morais <leonemorais.10@gmail.com>
2024-09-09 23:03:51 -0400

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo : "Cookie",
+        descricao : "Os cookies são biscoitos pequenos e macios, geralmente feitos com farinha, açúcar, ovos e manteiga. Existem inúmeras variações de cookies, com diferentes sabores e texturas, como chocolate, amendoim, aveia e muitos outros.",
+        link : "https://en.wikipedia.org/wiki/Cookie",
+        tags:"biscoito"
+    },
+    {
+        titulo : "Brownie",
+        descricao : "O brownie é um bolo denso e úmido, com sabor intenso de chocolate. Originário dos Estados Unidos, é uma sobremesa muito popular e versátil, podendo ser servido quente ou frio, com ou sem cobertura.",
+        link : "https://pt.wikipedia.org/wiki/Brownie_(bolo)",
+        tags:""
+    },
+
+    {
+        titulo : "Red Velvet",
+        descricao : "O bolo veludo vermelho, também conhecido pelo nome em inglês red velvet,[1] é um bolo popular, com uma coloração vermelha ou marrom-avermelhada. É usualmente preparada como um bolo de camadas, coberto com creme de baunilha ou, mais comum, butter cream.",
+        link : "https://pt.wikipedia.org/wiki/Bolo_veludo_vermelho",
+        tags:""
+    },
+];
